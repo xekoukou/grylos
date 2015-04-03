87e17bc0c4ac91feb83209764bfbcfac20ee3721
apostolis <apostolis@debian>
2015-04-03 09:16:48 -0400

    preinstall requires ribosome

--- preinstall.sh
+++ preinstall.sh
@@ -1,2 +1,2 @@
-command -v protein >/dev/null 2>&1 || { echo >&2 "proteinjs must be installed.('npm install proteinjs -g')  Aborting."; exit 1; }
+command -v ribosome.js >/dev/null 2>&1 || { echo >&2 "ribosome must be installed.('github.com/sustrik/ribosome')  Aborting."; exit 1; }
 command -v xmllint >/dev/null 2>&1 || { echo >&2 "xmllint must be installed.('libxml2-utils package in debian')  Aborting."; exit 1; }
